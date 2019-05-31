f98dede35ee6100999ed898e1e364de59d492ff0
jelly.xu <jellier@163.com>
2019-05-31 17:22:52 +0800

    plotting test

---
+++ Plot_test.py
@@ -0,0 +1,11 @@
+# matplotlib的用法
+# 使用前需要安装 pip3 install matplotlib
+# 安装过程中出现超时的错误，故重新安装了一遍
+
+# seaborn
+# pip3 install seaborn
+
+import matplotlib.pyplot as plt
+
+plt.plot([1,3,5] , [4,8, 10])
+plt.show()
